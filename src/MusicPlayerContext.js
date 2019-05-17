863814c602b2c8c40517b0c80b9728b843020338
Kelly de Vries <kdv@me.com>
2019-05-17 15:22:00 -0700

    add music files

--- src/MusicPlayerContext.js
+++ src/MusicPlayerContext.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import HappyRock from './bensound-happyrock.mp3';
+import JazzyFrenchy from './bensound-jazzyfrenchy.mp3';
+import Ukulele from './bensound-ukulele.mp3';
 const MusicPlayerContext = React.createContext([{}, () => {}]);
 
 const MusicPlayerProvider = props => {
     const [state, setState] = useState({
-        buttonName: 'Click me',
+        audioPlayer: new Audio(),
         tracks: [
-            { name: 'Story of My Life - One Direction' },
-            { name: 'Another One Bites the Dust - Queen' },
-            { name: 'Cough Syrup - Young the Giant' },
+            { name: 'Story of My Life - One Direction', file: HappyRock },
+            { name: 'Another One Bites the Dust - Queen', file: JazzyFrenchy },
+            { name: 'Cough Syrup - Young the Giant', file: Ukulele },
         ],
         currentTrackIndex: null,
         isPlaying: false,
